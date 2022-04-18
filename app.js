60636848e45e646c44d9511b82f83c185016144e
kathrynzaro <zarokathryn@gmail.com>
2022-04-18 15:03:49 -0700

    convertToSeconds function

--- app.js
+++ app.js
@@ -6,7 +6,9 @@
 // a number of minutes as an argument and returns
 // the number of seconds
 
-function convertToSeconds() {}
+function convertToSeconds(minutes) {
+    return minutes * 60;
+}
 
 console.log(`convertToSeconds(10) === 600 : ${convertToSeconds(10) === 600}`);
 
